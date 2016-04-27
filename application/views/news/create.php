dc97e96b903a68e576bf2f5fde4806aa443b09e0
Namit <namitm@sodelsolutions.com>
2016-04-27 16:01:04 +0530

    Added ckeditor into CI

--- application/views/news/create.php
+++ application/views/news/create.php
@@ -8,8 +8,19 @@
     <input type="input" name="title" /><br />
 
     <label for="text">Text</label>
-    <textarea name="text"></textarea><br />
+    <textarea name="text" rows="10" cols="80"></textarea><br />
 
     <input type="submit" name="submit" value="Create news item" />
+
+    <script>
+        // Replace the <textarea id="editor1"> with a CKEditor
+        // instance, using default configuration.
+        //$.noConflict();
+        $( document ).ready(function() {
+		    console.log( "ready!" );
+		    CKEDITOR.replace( 'text' );
+		});
+        
+    </script>
 
 </form>
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -4,9 +4,6 @@
         <img src="<?php echo base_url(); ?>assets/img/welspun-logo.png" class="img-responsive center-block">
       </div>
 </footer>
-
-    
-    <script src="<?php echo base_url(); ?>assets/js/jquery-1.9.1.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/owl.carousel.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/custom.js"></script>
   </body>
