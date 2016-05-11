ccf08e3ed766605efe21ca4a3989bcd041bf83e6
Namit <namitm@sodelsolutions.com>
2016-05-11 19:35:24 +0530

    Master : DB updates

--- uploads/codeignitor.sql
+++ uploads/codeignitor.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 05, 2016 at 07:39 PM
+-- Generation Time: May 11, 2016 at 07:34 PM
 -- Server version: 5.5.43-0ubuntu0.14.04.1
 -- PHP Version: 5.5.18-1+deb.sury.org~precise+1
 
@@ -54,10 +54,10 @@ CREATE TABLE IF NOT EXISTS `garvhai_players` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `profile_photo` varchar(255) NOT NULL,
-  `contest` varchar(128) NOT NULL,
+  `contest` varchar(500) NOT NULL,
   `championship` varchar(128) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
 
 --
 -- Dumping data for table `garvhai_players`
@@ -74,7 +74,75 @@ INSERT INTO `garvhai_players` (`id`, `name`, `profile_photo`, `contest`, `champi
 (9, 'Dipashree', 'ninja1.jpg', '', ''),
 (10, 'Dipashree', 'fido.jpg', '', ''),
 (12, 'Dipashree', 'fido1.jpg', '', ''),
-(13, 'Aryan', 'fido2.jpg', '<ul>\r\n	<li>Wimbeldon</li>\r\n	<li>Australian Open</li>\r\n	<li>Chennai Open</li>\r\n</ul>\r\n', '<ul>\r\n	<li>Olympics</li>\r\n</ul>\r\n');
+(13, 'Aryan Sanjay Pawashe', 'ninja11.jpg', '<ul>\r\n	<li>Wimbeldon</li>\r\n	<li>Australian Open</li>\r\n	<li>Chennai Open</li>\r\n	<li>US Open</li>\r\n	<li>French Open</li>\r\n	<li>Dubai Open</li>\r\n</ul>\r\n', '<ul>\r\n	<li>Olympics</li>\r\n	<li>Wimbledon</li>\r\n	<li>Grand slam</li>\r\n</ul>\r\n'),
+(14, 'sdfsdf', '659_17.jpg', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `garvhai_players_media`
+--
+
+CREATE TABLE IF NOT EXISTS `garvhai_players_media` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `player_id` int(10) NOT NULL,
+  `type` char(10) NOT NULL COMMENT 'image/video/social',
+  `media_value` varchar(500) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=47 ;
+
+--
+-- Dumping data for table `garvhai_players_media`
+--
+
+INSERT INTO `garvhai_players_media` (`id`, `player_id`, `type`, `media_value`, `timestamp`) VALUES
+(1, 13, 'image', 'ninja19.jpg', '2016-05-11 12:42:33'),
+(2, 13, 'image', 'fido12.jpg', '2016-05-11 12:42:34'),
+(3, 13, 'social', 'fido12.jpg', '2016-05-11 12:42:34'),
+(4, 13, 'social', 'fido12.jpg', '2016-05-11 12:42:34'),
+(5, 13, 'social', 'fido12.jpg', '2016-05-11 12:42:34'),
+(6, 13, 'image', 'ninja25.jpg', '2016-05-11 12:48:32'),
+(7, 13, 'image', 'fido18.jpg', '2016-05-11 12:48:32'),
+(8, 13, 'image', 'android-mobicon-150x1501.png', '2016-05-11 12:48:32'),
+(9, 13, 'video', 'android-mobicon-150x1501.png', '2016-05-11 12:48:32'),
+(10, 13, 'social', 'android-mobicon-150x1501.png', '2016-05-11 12:48:32'),
+(11, 13, 'social', 'android-mobicon-150x1501.png', '2016-05-11 12:48:32'),
+(12, 13, 'image', 'ninja26.jpg', '2016-05-11 12:49:18'),
+(13, 13, 'image', 'fido19.jpg', '2016-05-11 12:49:18'),
+(14, 13, 'image', 'android-mobicon-150x1502.png', '2016-05-11 12:49:18'),
+(15, 13, 'video', 'https://youtu.be/eZ_IOp638kQ', '2016-05-11 12:49:18'),
+(16, 13, 'social', 'Testing for social news', '2016-05-11 12:49:18'),
+(17, 13, 'social', 'Testing for social news', '2016-05-11 12:49:18'),
+(18, 13, 'image', 'ninja28.jpg', '2016-05-11 12:51:37'),
+(19, 13, 'image', 'fido21.jpg', '2016-05-11 12:51:37'),
+(20, 13, 'image', 'android-mobicon-150x1504.png', '2016-05-11 12:51:37'),
+(21, 13, 'video', 'https://youtu.be/eZ_IOp638kQ', '2016-05-11 12:51:37'),
+(22, 13, 'social', 'Testing for social news', '2016-05-11 12:51:37'),
+(23, 13, 'image', 'ninja29.jpg', '2016-05-11 13:18:46'),
+(24, 13, 'image', 'fido22.jpg', '2016-05-11 13:18:46'),
+(25, 13, 'image', 'android-mobicon-150x1505.png', '2016-05-11 13:18:47'),
+(26, 13, 'video', 'https://youtu.be/eZ_IOp638kQ', '2016-05-11 13:18:47'),
+(27, 13, 'social', 'Testing for social news', '2016-05-11 13:18:47'),
+(28, 13, 'image', 'ninja30.jpg', '2016-05-11 13:30:58'),
+(29, 13, 'image', 'fido23.jpg', '2016-05-11 13:30:58'),
+(30, 13, 'image', 'android-mobicon-150x1506.png', '2016-05-11 13:30:58'),
+(31, 13, 'video', 'https://youtu.be/eZ_IOp638kQ', '2016-05-11 13:30:58'),
+(32, 13, 'social', 'Testing for social news', '2016-05-11 13:30:58'),
+(33, 13, 'image', 'ninja31.jpg', '2016-05-11 13:31:46'),
+(34, 13, 'image', 'fido24.jpg', '2016-05-11 13:31:46'),
+(35, 13, 'image', 'android-mobicon-150x1507.png', '2016-05-11 13:31:46'),
+(36, 13, 'video', 'https://youtu.be/eZ_IOp638kQ', '2016-05-11 13:31:46'),
+(37, 13, 'social', 'Testing for social news', '2016-05-11 13:31:46'),
+(38, 13, 'image', 'Vserv-online-shopping-Mobile-apps-and-sites-featured.png', '2016-05-11 13:33:23'),
+(39, 13, 'video', 'https://youtu.be/eZ_IOp638kQ', '2016-05-11 13:33:23'),
+(40, 13, 'social', 'Testing for social news', '2016-05-11 13:33:23'),
+(41, 13, 'image', 'HPCMS-8677-EC4.png', '2016-05-11 13:50:48'),
+(42, 13, 'video', 'https://youtu.be/eZ_IOp638kQ', '2016-05-11 13:50:48'),
+(43, 13, 'social', 'Testing for social news', '2016-05-11 13:50:48'),
+(44, 13, 'image', 'HPCMS-8677-EC41.png', '2016-05-11 13:51:31'),
+(45, 13, 'video', 'https://youtu.be/eZ_IOp638kQ', '2016-05-11 13:51:32'),
+(46, 13, 'social', 'Testing for social news', '2016-05-11 13:51:32');
 
 -- --------------------------------------------------------
 
